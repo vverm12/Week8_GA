1f2b89b6fb55fec349b27cce4166a2c12229f75f
vverm12 <130741922+vverm12@users.noreply.github.com>
2023-04-19 13:24:35 +0530

    Update GA8Filecode.py

--- GA8Filecode.py
+++ GA8Filecode.py
@@ -1,3 +1,12 @@
+Import streamlit as st
+
+st.title("Find the largest among the 3 numbers")
+num1 = st.number_input("Enter number 1")
+num2 = st.number_input("Enter number 2")
+num3 = st.number_input("Enter number 3")
+calculate = st.button("Find largest number")
+
+
 def find_largest(a, b, c):
     largest = None
     if a > b and a > c:
@@ -7,3 +16,7 @@ def find_largest(a, b, c):
     else:
         largest = c
     return largest
+
+if calculate:
+    largest_num = largest_number(num1, num2, num3)
+    st.write("The largest number is:", largest_num)
